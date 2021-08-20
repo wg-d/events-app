b26e5e888c174f0e36b6ff357fa9d4f6da8c8b70
wg-d <wonderdream.dev@outlook.com>
2021-08-19 18:06:33 -0700

    added CORS setting in vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,12 @@
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false.valueOf,
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        ws: true,
+        changeOrigin: true
+      }
+    }
+  }
 }
